a7c61a1104467bde0293f3c35218a6648fe695b6
Tait Weicht <taitweicht@icloud.com>
2020-04-16 16:32:33 -0700

    Setup fetching from api

--- src/FriendsPage.tsx
+++ src/FriendsPage.tsx
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+interface Friend {
+  username: string;
+  status: FriendStatus;
+}
+
+enum FriendStatus {
+  accepted,
+  requested,
+}
 
 const FriendsPage: React.FC = () => {
+  const [error, setError] = useState("");
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [friends, setFriends] = useState<Friend[] | undefined>();
+
+  useEffect(() => {
+    fetch("https://paralibrary.digital/friends")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setFriends(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
   return <div>Friends page!</div>;
 };
 
