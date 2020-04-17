8daa6da3ec419af44daeffd60d6bb3add3ff854e
Tait Weicht <taitweicht@icloud.com>
2020-04-16 18:11:22 -0700

    Create a layout for pages. Might be nice for later [PAR-16]

---
+++ src/PageLayout.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from "styled-components";
+
+interface PageLayoutProps {
+  header?: React.ReactNode;
+  sidebar?: React.ReactNode;
+}
+
+const Layout = styled.div`
+  display: flow;
+  padding: 16px;
+  @media screen and (min-width: 480px) {
+    display: grid;
+    grid-template-columns: auto 50px;
+    column-gap: 16px;
+    row-gap: 16px;
+    grid-template-rows: auto;
+    grid-template-areas:
+      "header header"
+      "main sidebar";
+  }
+`;
+
+const Main = styled.div`
+  position: sticky;
+  @media screen and (min-width: 480px) {
+    grid-area: main;
+    position: static;
+    top: auto;
+  }
+`;
+
+const Header = styled.div`
+  grid-area: header;
+`;
+
+const Sidebar = styled.div`
+  grid-area: sidebar;
+`;
+
+const PageLayout: React.FC<PageLayoutProps> = ({
+  header,
+  sidebar,
+  children,
+}) => {
+  return (
+    <Layout>
+      {header && <Header>{header}</Header>}
+      <Main>{children}</Main>
+      {sidebar && <Sidebar>{sidebar}</Sidebar>}
+    </Layout>
+  );
+};
+
+export default PageLayout;
