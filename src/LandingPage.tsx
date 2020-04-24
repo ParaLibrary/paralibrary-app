6cef009c39107760eaad56447c3e2ebff6e833f3
hashiphoto <t.hashiphoto@gmail.com>
2020-04-24 14:22:24 -0700

    Landing page branding desktop and mobile [PAR-27]

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -1,35 +1,81 @@
 import React from "react";
 import styled from "styled-components";
-import { Image } from "react-bootstrap";
+import { GoogleLogin } from 'react-google-login';
 
 const LandingLayout = styled.div`
   display: grid;
   place-items: center;
-  grid-template-columns: 1fr 7fr 1fr;
-  grid-template-rows: 400px auto;
+
+  grid-template-columns: 1fr 10fr 1fr;
+  grid-template-rows: 200px 40px auto;
+  column-gap: 20px;
+  row-gap: 20px;
   grid-template-areas:
-    ". header ."
-    ". main. "
+    ". logo ."
+    ". title ."
+    ". main . ";
+
+  @media screen and (min-width: 480px) {
+    place-items: center;
+    grid-template-columns: 1fr 2fr 9fr 1fr;
+    grid-template-rows: 600px auto;
+    grid-template-areas:
+      ". logo title ."
+      ". main main . ";
+  }
 `;
 
-const ImageContainer = styled.div`
-  grid-area: header;
-  max-width: 1080px;
+const Logo = styled.img`
+  grid-area: logo;
+  align-self: end;
+  width: 50%;
+  display: block;
+  object-fit: contain;
+
+  @media screen and (min-width: 480px) {
+    align-self: center;
+    grid-area: logo;
+    width: 80%;
+    display: block;
+  }
 `;
 
-const SignInContainer = styled.div`
+const TitleText = styled.img`
+  grid-area: title;
+  align-self: start;
+  width: 100%;
+  display: block;
+  object-fit: contain;
+  
+  @media screen and (min-width: 480px) {
+    grid-area: title;
+    align-self: center;
+    width: 100%;
+    display: block;
+    object-fit: contain;
+  }
+`;
+
+const SignIn = styled.div`
   grid-area: main;
 `;
 
 const LandingPage: React.FC = () => {
   return (
     <LandingLayout>
-      <ImageContainer>
-        <Image src="/images/logo-full-black.png" fluid/>
-      </ImageContainer>
-      <SignInContainer>
-        Google
-      </SignInContainer>
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+    <meta name="google-signin-client_id" content="YOUR_CLIENT_ID.apps.googleusercontent.com"/>
+      <Logo src="/images/logo-icon-black.png" alt=""/>
+      <TitleText src="/images/logo-text-black.png"/>
+      <SignIn>
+        <GoogleLogin
+          clientId=""
+          buttonText="Sign in with Google"
+          onSuccess={() => {}}
+          onFailure={() => {}}
+          cookiePolicy={'single_host_origin'}
+        />
+      </SignIn>
     </LandingLayout>
   );
 };
