38d145c07d56eaee25144085f12e4bb39be93ae1
Tait Weicht <taitweicht@icloud.com>
2020-04-24 18:39:50 -0700

    Two more shallow tables [PAR-20] [PAR-21] [PAR-22]

--- src/LoansPage.tsx
+++ src/LoansPage.tsx
@@ -9,6 +9,8 @@ import { Button } from "react-bootstrap";
 const LoansPage: React.FC = () => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [loanedToMe, setLoanedToMe] = useState<Loan[]>([]);
+  const [loanedByMe, setLoanedByMe] = useState<Loan[]>([]);
+
   useEffect(() => {
     fetch("http://paralibrary.digital/api/loans/requester")
       .then((res) => {
@@ -29,14 +31,49 @@ const LoansPage: React.FC = () => {
       });
   }, []);
 
-  const borrowed: Loan[] = useMemo(
+  useEffect(() => {
+    fetch("http://paralibrary.digital/api/loans/owner")
+      .then((res) => {
+        setIsLoaded(true);
+        return res.json();
+      })
+      .then(
+        (result) => {
+          setLoanedByMe(result);
+        },
+        (error) => {
+          console.log(error);
+        }
+      )
+      .catch((error) => {
+        setIsLoaded(true);
+        console.log(error);
+      });
+  }, []);
+
+  const myBorrowing: Loan[] = useMemo(
     () => loanedToMe.filter((loan: Loan) => loan.status === "loaned"),
     [loanedToMe]
   );
 
-  const requested: Loan[] = useMemo(
+  const myRequests: Loan[] = useMemo(
     () => loanedToMe.filter((loan: Loan) => loan.status === "pending"),
     [loanedToMe]
+  );
+
+  const requestedFromMe: Loan[] = useMemo(
+    () => loanedByMe.filter((loan: Loan) => loan.status === "pending"),
+    [loanedByMe]
+  );
+
+  const loanedOut: Loan[] = useMemo(
+    () =>
+      loanedByMe
+        .filter(
+          (loan: Loan) => loan.status === "accepted" || loan.status === "loaned"
+        )
+        .sort((a: Loan, b: Loan) => Number(a.status > b.status)),
+    [loanedByMe]
   );
 
   return (
@@ -45,18 +82,41 @@ const LoansPage: React.FC = () => {
         "Loading..."
       ) : (
         <>
-          <AutoTable data={requested} title={<h3>Requested Books</h3>}>
+          <AutoTable
+            data={requestedFromMe}
+            title={<h3>Requested Books</h3>}
+            placeholder={
+              <div>
+                <span>No requests? </span>
+                <Link to={"/library"}>Add more books to your library!</Link>
+              </div>
+            }
+          >
+            <TableHeader col={"requester_id"}>Requester ID</TableHeader>
+            <span>wants</span>
+            <TableHeader col={"book_id"}>Book ID</TableHeader>
+          </AutoTable>
+
+          <AutoTable data={myRequests} title={<h3>My Requests</h3>}>
             <span>You requested</span>
             <TableHeader col={"book_id"}>Book ID</TableHeader>
             <span>from</span>
             <TableHeader col={"requester_id"}>Requester ID</TableHeader>
             <Button>Cancel Request?</Button>
           </AutoTable>
+
+          <AutoTable data={loanedOut} title={<h3>Loaned Books</h3>} hideOnEmpty>
+            <span>You've borrowed</span>
+            <TableHeader col={"book_id"}>Book ID</TableHeader>
+            <span>from</span>
+            <TableHeader col={"owner_id"}>Owner ID</TableHeader>
+          </AutoTable>
+
           <AutoTable
-            data={borrowed}
+            data={myBorrowing}
             title={<h3>Borrowed Books</h3>}
             placeholder={
-              requested.length === 0 ? (
+              myRequests.length === 0 ? (
                 <div>
                   <span>Doesn't look like you've borrowed any books.</span>
                   <br></br>
@@ -73,7 +133,7 @@ const LoansPage: React.FC = () => {
             <TableHeader col={"requester_id"}>Requester ID</TableHeader>
             <span>on</span>
             <TableHeader col={"loan_start_date"}>Loan Date</TableHeader>
-            <Button>Cancel Request?</Button>
+            <Button>Returned It!</Button>
           </AutoTable>
         </>
       )}
