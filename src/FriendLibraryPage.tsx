cf5d1d9603222d430176b2082e059eca8341424b
Tait Weicht <taitweicht@icloud.com>
2020-05-01 16:15:45 -0700

    Re-factor state management to prevent annoying flickering [PAR-9]

--- src/FriendLibraryPage.tsx
+++ src/FriendLibraryPage.tsx
@@ -10,6 +10,9 @@ const FriendLibraryPage: React.FC = () => {
 
   const [booksAreLoaded, setBooksAreLoaded] = useState(false);
   const [books, setBooks] = useState<Book[]>([]);
+  const [requests, setRequests] = useState<Loan[]>([]);
+  const [loansAreLoaded, setLoansAreLoaded] = useState(false);
+  const [user, setUser] = useState<User>();
   useEffect(() => {
     fetch(`http://paralibrary.digital/api/books`)
       .then((res) => {
@@ -32,8 +35,6 @@ const FriendLibraryPage: React.FC = () => {
       });
   }, []);
 
-  const [requests, setRequests] = useState<Loan[]>([]);
-  const [loansAreLoaded, setLoansAreLoaded] = useState(false);
   useEffect(() => {
     fetch("http://paralibrary.digital/api/loans/requester")
       .then((res) => {
@@ -55,7 +56,6 @@ const FriendLibraryPage: React.FC = () => {
       });
   }, []);
 
-  const [user, setUser] = useState<User>();
   useEffect(() => {
     fetch(`http://paralibrary.digital/api/users/${id}`)
       .then((res) => {
@@ -74,41 +74,34 @@ const FriendLibraryPage: React.FC = () => {
       });
   }, [id]);
 
-  return (
-    <PageLayout
-      header={
-        !user ? (
-          <h1>Library Not Found</h1>
-        ) : (
-          <h1>{user.display_name}'s Library</h1>
-        )
-      }
-    >
+  return !booksAreLoaded ? (
+    <span>Loading...</span>
+  ) : (
+    <PageLayout header={!user ? null : <h1>{user.display_name}'s Library</h1>}>
       {!!user && (
-        <div>
+        <>
           {!loansAreLoaded && (
             <span>
               Error: Unable to check if you have any current loans from this
               user.
             </span>
           )}
-          {!booksAreLoaded || !user ? (
-            "Loading books..."
-          ) : (
-            <AutoTable
-              data={books}
-              title={<h3>Books</h3>}
-              placeholder={
-                <span>
-                  Huh, looks like this {user.display_name} hasn't added anything
-                  to their library.
-                </span>
-              }
-            >
-              <TableHeader col="title">Title</TableHeader>
-            </AutoTable>
-          )}
-        </div>
+
+          <AutoTable
+            data={books}
+            title={<h3>Books</h3>}
+            placeholder={
+              <span>
+                Huh, looks like {user.display_name} hasn't added anything to
+                their library.
+              </span>
+            }
+          >
+            <TableHeader col="title">Title</TableHeader>
+            <TableHeader col="author">Author</TableHeader>
+            <TableHeader col="summary">Description</TableHeader>
+          </AutoTable>
+        </>
       )}
     </PageLayout>
   );
