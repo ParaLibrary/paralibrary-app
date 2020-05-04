89bd15eb47379129af80faf7f7645497933ba496
Tait Weicht <taitweicht@icloud.com>
2020-05-03 23:27:26 -0700

    Simpler loan request object [PAR-13]

--- src/ourtypes.ts
+++ src/ourtypes.ts
@@ -5,16 +5,23 @@ export interface Friend {
   status: "requested" | "friends";
 }
 
-export interface Loan {
+export interface Loan extends LoanRequest {
   id: number;
   owner_id: number;
+  owner_contact: string;
   requester_id: number;
+  requester_contact: string;
   book_id: number;
   request_date: Date;
   accept_date: Date;
   loan_start_date: Date;
   loan_end_date: Date;
   status: "pending" | "accepted" | "loaned" | "returned" | "late";
+}
+
+export interface LoanRequest {
+  book_id: number;
+  requester_contact: string;
 }
 
 export interface Book {
