dc201184da888857cd8e9a2ccbdbc06645167183
Tait Weicht <taitweicht@icloud.com>
2020-05-06 16:36:39 -0700

    Use new libraries route [PAR-13]

--- src/FriendLibraryPage.tsx
+++ src/FriendLibraryPage.tsx
@@ -11,30 +11,21 @@ import LoanFormik from "./LoanForm";
 const FriendLibraryPage: React.FC = () => {
   const { id } = useParams();
 
-  const [booksAreLoaded, setBooksAreLoaded] = useState(false);
-  const [books, setBooks] = useState<Book[]>([
-    {
-      id: 1,
-      user_id: 1,
-      title: "Test",
-      author: "testy test",
-      isbn: "",
-      summary: "",
-      private: true,
-    },
-  ]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [books, setBooks] = useState<Book[]>([]);
   const [requests, setRequests] = useState<Loan[]>([]);
-  const [loansAreLoaded, setLoansAreLoaded] = useState(false);
   const [user, setUser] = useState<User>();
   useEffect(() => {
-    fetch(`http://paralibrary.digital/api/books`)
+    fetch(`http://paralibrary.digital/api/libraries/${id}`)
       .then((res) => {
         console.log(res);
         return res.json();
       })
       .then(
         (result) => {
-          setBooks(result as Book[]);
+          setBooks(result.books);
+          setUser(result.user);
+          console.log(result);
         },
         (error) => {
           console.log(error);
@@ -44,48 +35,9 @@ const FriendLibraryPage: React.FC = () => {
         console.log(error);
       })
       .finally(() => {
-        setBooksAreLoaded(true);
+        setIsLoaded(true);
       });
   }, []);
-
-  useEffect(() => {
-    fetch("http://paralibrary.digital/api/loans/requester")
-      .then((res) => {
-        return res.json();
-      })
-      .then(
-        (result) => {
-          setRequests(result);
-        },
-        (error) => {
-          console.log(error);
-        }
-      )
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => {
-        setLoansAreLoaded(true);
-      });
-  }, []);
-
-  useEffect(() => {
-    fetch(`http://paralibrary.digital/api/users/${id}`)
-      .then((res) => {
-        return res.json();
-      })
-      .then(
-        (result) => {
-          setUser(result);
-        },
-        (error) => {
-          console.log(error);
-        }
-      )
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [id]);
 
   const [modalOpen, setModalOpen] = useState(false);
   const [isNewRequest, setIsNewRequest] = useState(true);
@@ -121,42 +73,29 @@ const FriendLibraryPage: React.FC = () => {
     }).then((res) => res.ok);
   }, []);
 
-  return !booksAreLoaded ? (
-    <span>Loading...</span>
-  ) : (
+  return (
     <PageLayout header={!user ? null : <h1>{user.display_name}'s Library</h1>}>
-      {!!user && (
-        <>
-          {!loansAreLoaded && (
-            <span>
-              Error: Unable to check if you have any current loans from this
-              user.
-            </span>
-          )}
-
-          <AutoTable
-            data={books}
-            title={<h3>Books</h3>}
-            placeholder={
-              <span>
-                Huh, looks like {user.display_name} hasn't added anything to
-                their library.
-              </span>
-            }
-          >
-            <TableHeader col="title">Title</TableHeader>
-            <TableHeader col="author">Author</TableHeader>
-            <TableHeader col="summary">Description</TableHeader>
-            <LoanRequestButton
-              userID={1}
-              id={0}
-              loans={requests}
-              onRequest={handleRequest}
-              onCancel={handleCancel}
-            />
-          </AutoTable>
-        </>
-      )}
+      <AutoTable
+        data={books}
+        title={<h3>Books</h3>}
+        placeholder={
+          <span>
+            Huh, looks like {user && user.display_name} hasn't added anything to
+            their library.
+          </span>
+        }
+      >
+        <TableHeader col="title">Title</TableHeader>
+        <TableHeader col="author">Author</TableHeader>
+        <TableHeader col="summary">Description</TableHeader>
+        <LoanRequestButton
+          userID={1}
+          id={0}
+          loans={requests}
+          onRequest={handleRequest}
+          onCancel={handleCancel}
+        />
+      </AutoTable>
       <Modal show={modalOpen} onHide={() => setModalOpen(false)} centered>
         <Modal.Header closeButton>
           <Modal.Title>
