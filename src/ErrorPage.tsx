82f9f68ea4df141e3274685defa8af802ae79cc6
Tait Weicht <taitweicht@icloud.com>
2020-05-23 17:11:41 -0700

    Add fun quote [PAR-33]

--- src/ErrorPage.tsx
+++ src/ErrorPage.tsx
@@ -1,12 +1,36 @@
 import React from "react";
+import Alert from "react-bootstrap/Alert";
+import styled from "styled-components";
+
 import PageLayout from "./PageLayout";
 
-import Alert from "react-bootstrap/Alert";
+const FlowDown = styled.div`
+  height: 100%;
+  display: flex;
+  flex-flow: column;
+  justify-content: space-between;
+`;
 
 const ErrorPage: React.FC = () => {
   return (
     <PageLayout header={<h1>Fahrenheit 404</h1>} loaded>
-      Seems like there's nothing much to see here.
+      <FlowDown>
+        It seems this page does not exist.
+        <Alert variant="primary">
+          <span>
+            “Would you tell me, please, which way I ought to go from here?”
+            <br />
+            “That depends a good deal on where you want to get to,” said the
+            Cat. <br />
+            “I don’t much care where—” said Alice. <br /> “Then it doesn’t
+            matter which way you go,” said the Cat.
+          </span>
+          <hr />
+          <span>
+            – Lewis Carrol, <em>Alice's Adventures in Wonderland</em>
+          </span>
+        </Alert>
+      </FlowDown>
     </PageLayout>
   );
 };
